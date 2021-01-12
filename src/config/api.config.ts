2917059d7d8462441a3a44afd539d5c510ff2020
Welton Carvalho <weltoncarvalhoo@gmail.com>
2021-01-12 13:26:23 -0300

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://zvelto-curso-spring.herokuapp.com",
     bucketBaseUrl: "https://curso-spring-ionic-zvelto.s3-sa-east-1.amazonaws.com"
 }
